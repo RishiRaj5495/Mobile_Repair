cdfaecac4dc1de829bbb6ec72c6973d8b29ee379
9307159365raj@gmail.com <9307159365raj@gmail.com>
2026-01-15 00:06:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ const io = require("socket.io")(server, {
 //connect sockets
 socketSetup(io);
 
-// const dbUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mobile-repair-services';
-const dbUrl = 'mongodb://127.0.0.1:27017/mobile-repair-services';
+const dbUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mobile-repair-services';
+// const dbUrl = 'mongodb://127.0.0.1:27017/mobile-repair-services';
 main()
   .then(() => console.log("MongoDB connection successful"))
   .catch((err) => console.log("DB error:", err));
